83fe851944699f832026b00caae376537c44af71
RawalAnuma <rawalanuma@gmail.com>
2025-06-25 11:51:16 +0545

    constrctor added

--- src/ATMAccount.java
+++ src/ATMAccount.java
@@ -2,4 +2,12 @@ public class ATMAccount {
     private int accountNumber;
     private String accountHolderName;
     private double balance;
+    private int pinNumber;
+
+    public ATMAccount(int accountNumber, String accountHolderName, double initialBalance, int pinNumber) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+        this.pinNumber = pinNumber;
+    }
 }
