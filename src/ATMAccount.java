75a8ee8ec71c67339494faf12c91b68feab66028
RawalAnuma <rawalanuma@gmail.com>
2025-06-25 12:14:30 +0545

    methods added

--- src/ATMAccount.java
+++ src/ATMAccount.java
@@ -4,10 +4,34 @@ public class ATMAccount {
     private double balance;
     private int pinNumber;
 
-    public ATMAccount(int accountNumber, String accountHolderName, double initialBalance, int pinNumber) {
+    public ATMAccount(int accountNumber, double initialBalance, int pinNumber) {
         this.accountNumber = accountNumber;
-        this.accountHolderName = accountHolderName;
         this.balance = initialBalance;
         this.pinNumber = pinNumber;
     }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0){
+            balance += amount;
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance = balance - amount;
+        } else{
+            System.out.println("Withdrawal amount must be positive and less than or equal to the balance.");
+        }
+    }
+
 }
---
+++ src/ATMSimulator.java
@@ -0,0 +1,5 @@
+public class ATMSimulator {
+    public static void main(String[] args) {
+
+    }
+}
